dd58192a002347fb8d63982f5d470d23e1954204
dingobeo250800 <Dinhnguyen250800@gmail.com>
2022-07-07 14:51:22 +0700

    Cau hinh Router/Layout cho du an

---
+++ src/components/pages/Profile/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Profile(props) {
+    return <div>Profile Pages</div>;
+}
+
+export default Profile;
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import { HeaderOnly } from '~/components/Layout';
+import Following from '~/components/pages/Following';
+import Home from '~/components/pages/Home';
+import Profile from '~/components/pages/Profile';
+import Search from '~/components/pages/Search';
+import Upload from '~/components/pages/Upload';
+
+//public routes
+export const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+export const privateRoutes = [];
