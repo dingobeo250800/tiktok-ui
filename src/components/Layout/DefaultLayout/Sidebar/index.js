b552061b87d2fcac7a406d8fdde4a5458ec8fcf9
dingobeo250800 <Dinhnguyen250800@gmail.com>
2022-07-08 17:21:58 +0700

    dung khung layout macdinh

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import className from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = className.bind(styles);
 
 function Sidebar(props) {
-    return <div>Sidebar</div>;
+    return <aside className={cx('wrapper')}>sidebar</aside>;
 }
 
 export default Sidebar;
