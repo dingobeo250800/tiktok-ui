3cf2012fc97034d8e90123be5e37c24b01f43d13
dingobeo250800 <Dinhnguyen250800@gmail.com>
2022-07-21 11:58:24 +0700

    Xây dựng UI phần Header #3

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+
+function AccountItem(props) {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://images.unsplash.com/photo-1563365488-1e365499ba88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt="Natrue"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyễn Văn Dinh</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>Dinhnguyen</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
