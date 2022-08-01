479c1fada59de3d9d19b72c917f8a0f7c539120b
dingobeo250800 <Dinhnguyen250800@gmail.com>
2022-08-01 14:55:11 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import React from 'react';
+import Header from '../components/Header';
+import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+import className from 'classnames/bind';
+const cx = className.bind(styles);
+
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
+            </div>
+        </div>
+    );
+}
+DefaultLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+export default DefaultLayout;
